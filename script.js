949ab989fbaeb4f87ac5c5374216b3ec46ad5344
Julia1284 <julia123456-84@mail.ru>
2021-09-04 11:28:42 +0300

    commit

---
+++ script.js
@@ -0,0 +1,25 @@
+
+function calcSum(){
+   let digit1 = prompt("Введите первое число");
+   let digit2 = prompt("Введите второe число");
+   let result = Number(digit1)+Number(digit2);// преобразуем в число
+   alert(result);
+}
+function calcSubtract(){
+   let digit1 = prompt("Введите первое число");
+   let digit2 = prompt("Введите второe число");
+   let result = Number(digit1)-Number(digit2);// преобразуем в число
+   alert(result);
+}
+function calcDiv(){
+   let digit1 = prompt("Введите первое число");
+   let digit2 = prompt("Введите второe число");
+   let result = Number(digit1)/Number(digit2);// преобразуем в число
+   alert(result);
+}
+function calcMulty(){
+   let digit1 = prompt("Введите первое число");
+   let digit2 = prompt("Введите второe число");
+   let result = Number(digit1)*Number(digit2);// преобразуем в число
+   alert(result);
+}
