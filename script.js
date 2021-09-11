d6978d04b8c8581ae20143c6389a935405528f84
Julia1284 <julia123456-84@mail.ru>
2021-09-11 11:08:33 +0300

    Commit1

--- script.js
+++ script.js
@@ -1,25 +1,27 @@
 
 function calcSum(){
-   let digit1 = prompt("Введите первое число");
-   let digit2 = prompt("Введите второe число");
-   let result = Number(digit1)+Number(digit2);// преобразуем в число
-   alert(result);
+   let digit1 = document.getElementById('input1');
+   let digit2 = document.getElementById('input2');
+   let result = Number(digit1.value)+Number(digit2.value);// преобразуем в число
+   document.getElementById ('resultsum').innerHTML=result;
 }
+
+
 function calcSubtract(){
-   let digit1 = prompt("Введите первое число");
-   let digit2 = prompt("Введите второe число");
-   let result = Number(digit1)-Number(digit2);// преобразуем в число
-   alert(result);
+   let digit1 = document.getElementById('input3');
+   let digit2 = document.getElementById('input4');
+   let result = Number(digit1.value)-Number(digit2.value);// преобразуем в число
+   document.getElementById ('resultsubtract').innerHTML=result;
 }
 function calcDiv(){
-   let digit1 = prompt("Введите первое число");
-   let digit2 = prompt("Введите второe число");
-   let result = Number(digit1)/Number(digit2);// преобразуем в число
-   alert(result);
+   let digit1 = document.getElementById('input5');
+   let digit2 = document.getElementById('input6');
+   let result = Number(digit1.value)/Number(digit2.value);// преобразуем в число
+   document.getElementById ('resultdivision').innerHTML=result;
 }
 function calcMulty(){
-   let digit1 = prompt("Введите первое число");
-   let digit2 = prompt("Введите второe число");
-   let result = Number(digit1)*Number(digit2);// преобразуем в число
-   alert(result);
+   let digit1 = document.getElementById('input7');
+   let digit2 = document.getElementById('input8');
+   let result = Number(digit1.value)*Number(digit2.value);// преобразуем в число
+   document.getElementById ('resultmulty').innerHTML=result;
 }
