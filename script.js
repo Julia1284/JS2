9e76329b2019f7e6d58cfeeb4b8de199725c0950
Julia1284 <julia123456-84@mail.ru>
2021-09-17 22:04:20 +0300

    commit2

--- script.js
+++ script.js
@@ -1,27 +1,31 @@
 
 function calcSum(){
-   let digit1 = document.getElementById('input1');
-   let digit2 = document.getElementById('input2');
-   let result = Number(digit1.value)+Number(digit2.value);// преобразуем в число
+   let digit1 = Number(document.getElementById('input1').value);
+   let digit2 = Number(document.getElementById('input2').value);
+   let result = digit1+digit2;
    document.getElementById ('resultsum').innerHTML=result;
 }
 
-
 function calcSubtract(){
-   let digit1 = document.getElementById('input3');
-   let digit2 = document.getElementById('input4');
-   let result = Number(digit1.value)-Number(digit2.value);// преобразуем в число
+   let digit1 = Number(document.getElementById('input3').value);
+   let digit2 = Number(document.getElementById('input4').value);
+   let result = digit1-digit2;
    document.getElementById ('resultsubtract').innerHTML=result;
 }
 function calcDiv(){
-   let digit1 = document.getElementById('input5');
-   let digit2 = document.getElementById('input6');
-   let result = Number(digit1.value)/Number(digit2.value);// преобразуем в число
-   document.getElementById ('resultdivision').innerHTML=result;
+   let digit1 = Number(document.getElementById('input5').value);
+   let digit2 = Number(document.getElementById('input6').value);
+   let result = digit1/digit2;
+   if (digit2 == 0) {
+      document.getElementById ('resultdivision').innerHTML = 'На ноль делить нельзя!';
+   }
+   else{
+      document.getElementById ('resultdivision').innerHTML=result;
+   }  
 }
 function calcMulty(){
-   let digit1 = document.getElementById('input7');
-   let digit2 = document.getElementById('input8');
-   let result = Number(digit1.value)*Number(digit2.value);// преобразуем в число
+   let digit1 = Number(document.getElementById('input7').value);
+   let digit2 = Number(document.getElementById('input8').value);
+   let result = digit1*digit2;
    document.getElementById ('resultmulty').innerHTML=result;
 }
